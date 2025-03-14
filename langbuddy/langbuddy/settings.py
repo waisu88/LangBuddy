f6b3db7f9fb271f2afca148bc8b8c1b067bc6de6
Szymon <szymon.wais@gmail.com>
2025-03-14 22:25:52 +0100

    Zmienone settings-dodane zależności restfr, + dodane url

--- langbuddy/langbuddy/settings.py
+++ langbuddy/langbuddy/settings.py
@@ -53,6 +53,13 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+}
+
+
 ROOT_URLCONF = 'langbuddy.urls'
 
 TEMPLATES = [
