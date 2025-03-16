b569ffb32b93ac1bcab652c6d54e454593210fd6
Szymon <szymon.wais@gmail.com>
2025-03-16 21:40:26 +0100

    Pierwszy api view i JsonResponse

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,7 +1,7 @@
 import requests
 
 # endpoint = "https://httpbin.org/status/200/"
-endpoint = "http://localhost:8000/"
+endpoint = "http://localhost:8000/api/"
 
 get_response = requests.get(endpoint, json={"query": "hello world"})
 print(get_response.text)
