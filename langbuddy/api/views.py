0a63785d5c8d25522336064789d6b37a0275f09a
Szymon <szymon.wais@gmail.com>
2025-03-18 13:34:43 +0100

    SKonczone api-view

--- langbuddy/api/views.py
+++ langbuddy/api/views.py
@@ -1,5 +1,21 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+import json
+from django.forms.models import model_to_dict
+from products.models import Product
 
 
+
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+
+@api_view(["GET", "POST"])
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "Hi there"})
+    if request.method != "POST":
+        return Response({"detail": "GET not allowed"}, status=405)
+    model_data = Product.objects.all().order_by("?").first()
+    data = {}
+    if model_data:
+        data = model_to_dict(model_data, fields=["id", "title"])
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -3,8 +3,9 @@ import requests
 # endpoint = "https://httpbin.org/status/200/"
 endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint, json={"query": "hello world"})
-print(get_response.text)
-print(get_response.status_code)
+get_response = requests.get(endpoint, params={"abc": 123}, json={"query": "hello world"})
+# print(get_response.text)
+# print(get_response.status_code)
 # print(get_response.json())
 
+print(get_response.json())
